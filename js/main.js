c402b63e582aa337eb5c27ae3674b9fc75495e08
ErbekovBobur <115180724+ErbekovBobur@users.noreply.github.com>
2025-05-10 03:33:31 +0500

    faq add

--- js/main.js
+++ js/main.js
@@ -142,6 +142,26 @@ document.addEventListener("DOMContentLoaded", function () {
       }
     });
   });
+  // FAQ
+  const faqItems = document.querySelectorAll('.faq-item');
+
+  faqItems.forEach(item => {
+    const question = item.querySelector('.faq-question');
+
+    question.addEventListener('click', () => {
+      // Закрываем все другие вопросы, если они были открыты
+      faqItems.forEach(innerItem => {
+        if (innerItem !== item) {
+          innerItem.classList.remove('active');
+        }
+      });
+
+      // Переключаем текущий вопрос
+      item.classList.toggle('active');
+    });
+  });
+  
+  
 });
 
 // --- Service Worker ---
