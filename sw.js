584e4406271f995237e6904b7466883dd7372b5f
ErbekovBobur <erbekov1985@gmail.com>
2025-05-24 09:08:05 +0500

    update path

--- sw.js
+++ sw.js
@@ -19,22 +19,22 @@ const urlsToCache = [
   "img/hero-banner.avif",
 ];
 
-self.addEventListener("install", (event) => {
-  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache)));
-});
+// self.addEventListener("install", (event) => {
+//   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache)));
+// });
 
-self.addEventListener("fetch", (event) => {
-  if (event.request.mode === "navigate") {
-    event.respondWith(fetch(event.request).catch(() => caches.match(OFFLINE_URL)));
-  } else {
-    event.respondWith(caches.match(event.request).then((response) => response || fetch(event.request)));
-  }
-});
+// self.addEventListener("fetch", (event) => {
+//   if (event.request.mode === "navigate") {
+//     event.respondWith(fetch(event.request).catch(() => caches.match(OFFLINE_URL)));
+//   } else {
+//     event.respondWith(caches.match(event.request).then((response) => response || fetch(event.request)));
+//   }
+// });
 
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(cacheNames.filter((name) => name !== CACHE_NAME).map((name) => caches.delete(name)));
-    })
-  );
-});
+// self.addEventListener("activate", (event) => {
+//   event.waitUntil(
+//     caches.keys().then((cacheNames) => {
+//       return Promise.all(cacheNames.filter((name) => name !== CACHE_NAME).map((name) => caches.delete(name)));
+//     })
+//   );
+// });
