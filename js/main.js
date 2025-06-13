87e8dac54f6bb4b2261b5460bb8065f7f186c291
ErbekovBobur <erbekov1985@gmail.com>
2025-06-13 14:51:47 +0500

    update

--- js/main.js
+++ js/main.js
@@ -173,7 +173,6 @@ document.addEventListener("DOMContentLoaded", function () {
         // header.style.background = "rgba(255, 255, 255, 0.98)";
         header.style.boxShadow = "0 2px 20px rgba(0, 0, 0, 0.1)";
       }
-
       lastScroll = currentScroll;
     }, 50)
   );
